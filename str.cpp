67be5f1df8d748fe78bc485d8b14d933da683178
Narayan <singhrathorenarayan94@gmail.com>
2026-06-23 10:10:44 +0530

    Updated str.cpp

--- str.cpp
+++ str.cpp
@@ -16,23 +16,46 @@
 
 // }
 
-#include<iostream>
-using namespace std;
-int main (){
-    string b = "narayan";
-    for (int i =0 ;i<=b.length() ; i++) {
-        if (b[i]=='a'){
-            b++;
-           // cout<<b;
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main () { 
+//     string b = "narayan";
+//     for (int i =0 ;i<=b.length() ; i++) {
+//         if (b[i]=='a'){
+//             b++;
+//            // cout<<b;
 
             
-        }
+//         }
 
         
 
-    }
-    cout<<b;
+//     }
+//     cout<<b;
 
-    return 0;
+//     return 0;
 
-}
+// }
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main () {
+//     string b="narayan";
+//     int c=b.length();
+//     string e ="";
+//     for (int i=0; i<b.length(); i++) {
+//         if (i >= b.length() - 3) {
+//             e=e+b[i];
+
+//         }
+//     }
+//     cout<<e<<endl;
+//     return 0;
+
+// }
